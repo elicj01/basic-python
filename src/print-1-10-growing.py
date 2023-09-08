023d7402dfdbeef70db02b57f2eeceac0ac399a4
Elias Calandra <eliascalandrajimenez@gmail.com>
2023-09-08 09:25:37 +0200

    .\src\print-1-10-growing.py

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,2 +1,9 @@
 
 # Print the numbers described in the exercise
+
+sequence = []
+
+for i in range(1, 11):
+    sequence.append(str(i))
+    sequence_str = ' '.join(sequence)
+    print(sequence_str)
