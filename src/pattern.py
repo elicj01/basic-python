a9bbaa9152517ef67ce5455751246fe65a6e6b8e
Elias Calandra <eliascalandrajimenez@gmail.com>
2023-09-08 09:28:45 +0200

    .\src\pattern.py

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,14 @@
 
 # Print the pattern
+
+sequence = []
+
+for i in range(1, 6):
+    sequence.append(str("*"))
+    sequence_str = ' '.join(sequence)
+    print(sequence_str)
+
+for i in range(6, 1, -1):
+    sequence.pop()
+    sequence_str = ' '.join(sequence)
+    print(sequence_str)
